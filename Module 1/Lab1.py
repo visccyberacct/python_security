a8279ef0c37999b2c98bae75b03bf597b6b8a244
Kent <visscyberacct@gmail.com>
2023-07-05 12:49:26 -0500

    Added first lab to respository

---
+++ Module 1/Lab1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+# This line displays "Hello world!"
+print("Hello world!")
+
+# This line is to work with comments and print()
+print("I am scripting in Python")
+
+# This cell displays "I am a security analyst."
+print("I am a security analyst")
+
+# This cell displays "Python is useful for security!"
+print("Python is useful for security")
